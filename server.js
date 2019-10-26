949dbc81de7a1f773114a7fe9353064db5e55c34
Mayvis <kevin73911@gmail.com>
2019-10-26 23:39:13 +0800

    validate user

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+// Connect database
+connectDB();
+
+// Init middleware
+app.use(express.json());
 
 app.get("/", (req, res) => res.json({ message: "Welcome to the ContactKeeper app." }));
 
 // Define routes
 app.use("/api/users", require('./routes/users'));
-app.use("/api/auth", require('./routes/auth'));
-app.use("/api/contacts", require('./routes/contacts'));
+// app.use("/api/auth", require('./routes/auth'));
+// app.use("/api/contacts", require('./routes/contacts'));
 
 app.listen(PORT, () => console.log(`Server starts on PORT ${PORT}`));
